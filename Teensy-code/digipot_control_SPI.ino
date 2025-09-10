f46bd98ac8ed32fe096ca47276752cda61c30fd9
Reuben Betts <30935412+reubenwbetts@users.noreply.github.com>
2025-09-10 13:09:38 +0100

    Update digipot_control_SPI.ino

--- Teensy-code/digipot_control_SPI.ino
+++ Teensy-code/digipot_control_SPI.ino
@@ -1,14 +1,19 @@
 #include <SPI.h>
 #include "one_pole_iir.h"
 
-uint8_t input_pot_values[8] = {   };
+uint8_t input_pot_values[8] = {127,127,127,127,127,127,127,127   };
 uint8_t digipot_values_past[12] = {   };
 uint8_t digipot_values[12] = {   };
 uint8_t input_pots[8] = {16,17,18,19,20,21,22,23};
 
 one_pole_iir potfilter[16];
 uint8_t filter_input;
-float input_pot_damp_val = 0.5; // massively overdamped, but good for visualising on plotter
+
+/* 
+using Q8.8, so 255 = overdamped, 0 = nodamping
+filter is in fixed-point, and wanted to keep filters super duper light so it uses 16 bit resolution inside the filter. this causes a DC sag at high damping values, so don't put them tooo high (>150).
+  */
+uint16_t input_pot_damp_val = 150; 
 
 EventResponder spiDone;
 volatile bool spiBusy = false;
@@ -20,6 +25,7 @@ void spiFinished(EventResponderRef event) {
 
 void setup() {
   Serial.begin(9600);
+  analogReadResolution(8);
   // set the slaveSelectPin as an output:
   pinMode(SS, OUTPUT);
   digitalWriteFast(SS, HIGH);
@@ -40,8 +46,11 @@ void loop() {
 
   for (int i = 0; i < 8; i++){
 
-    // shift to 8-bits
-    input_pot_values[i] = analogRead(input_pots[i]) >> 2; 
+    /* read pots - analogRead is the bottleneck 
+    in this code, need to replace it */
+    
+
+    input_pot_values[i] = analogRead(input_pots[i]); 
     
     // filter input pots with two single pole IIR's, should do a second order but didn't have time.
     input_pot_values[i] = potfilter[i].filter_method(input_pot_values[i],input_pot_damp_val);  
@@ -58,6 +67,12 @@ void loop() {
 
   }
 
+  Serial.print("baseline:");
+  Serial.println(0);
+  Serial.print("filtered:");
+  Serial.println(digipot_values[11]);
+  Serial.print("topline:");
+  Serial.println(255);
   // check for any changed values and update past values to current
   for (int i = 0; i<12; i++){
     if (digipot_values_past[i] != digipot_values[i])
@@ -73,5 +88,3 @@ void loop() {
   }
 
 }
-
-
