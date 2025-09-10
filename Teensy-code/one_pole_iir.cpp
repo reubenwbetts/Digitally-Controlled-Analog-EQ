b0d4e0114c428375b99d6c810f266ada37a51d0a
Reuben Betts <30935412+reubenwbetts@users.noreply.github.com>
2025-09-10 13:10:22 +0100

    Update one_pole_iir.cpp

--- Teensy-code/one_pole_iir.cpp
+++ Teensy-code/one_pole_iir.cpp
@@ -2,10 +2,10 @@
 #include <cstdint>
 #include <cmath>
 
- uint8_t one_pole_iir::filter_method(uint8_t x, float damp_factor)
+ uint8_t one_pole_iir::filter_method(uint8_t x, uint16_t damp_factor)
 {
-  float y = (1.0f - damp_factor) * x + damp_factor * y_prev;
-  y_prev = y;
-  uint8_t output = static_cast<uint8_t>(y);
-  return output;
+    // add 128 to fix DC sag (roudning errors from bit-shifting)
+    uint16_t y = ((256-damp_factor) * x + damp_factor * y_prev + 128) >> 8;
+    y_prev = static_cast<uint8_t>(y);
+    return y_prev;
 }
